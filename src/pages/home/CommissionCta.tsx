da9dd569c37c980ff71209a173d4454177e279eb
c22he <c22he@uwaterloo.ca>
2026-08-23 19:57:59 -0400

    small tweaks

--- src/pages/home/CommissionCta.tsx
+++ src/pages/home/CommissionCta.tsx
@@ -15,7 +15,7 @@ export function CommissionCta() {
 
             <a
               href="https://yumicommissions.carrd.co/#"
-              className="mt-5 inline-flex items-center gap-2 text-sm text-neutral-700 hover:text-[#356cff]"
+              className="mt-5 inline-flex items-center gap-2 text-lg text-neutral-700 hover:text-[#356cff]"
             >
               <span>↗</span>
               Commission Info
