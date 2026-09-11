99ca64996bfd358b1e63bdaf64a593d16d06c476
c22he <c22he@uwaterloo.ca>
2026-09-11 09:08:54 -0400

    fixing ts errors for deployment

--- src/data/coolStuff.ts
+++ src/data/coolStuff.ts
@@ -5,23 +5,23 @@ import mirrorBlindCover from "../assets/images/cool-stuff/mirror-blind-cover.png
 import animationCover from "../assets/images/cool-stuff/animation-cover.png";
 import canvasCover from "../assets/images/cool-stuff/canvas-cover.jpeg";
 
-import tctcLogo from "../assets/images/cool-stuff/tctc-logo.png";
-import tctcFinal from "../assets/images/cool-stuff/tctc-final-design.png";
-import tctcSketch from "../assets/images/cool-stuff/tctc-sketch.png";
-import tctcMock1 from "../assets/images/cool-stuff/tctc-mock-1.jpeg";
-import tctcMock2 from "../assets/images/cool-stuff/tctc-mock-2.jpeg";
+// import tctcLogo from "../assets/images/cool-stuff/tctc-logo.png";
+// import tctcFinal from "../assets/images/cool-stuff/tctc-final-design.png";
+// import tctcSketch from "../assets/images/cool-stuff/tctc-sketch.png";
+// import tctcMock1 from "../assets/images/cool-stuff/tctc-mock-1.jpeg";
+// import tctcMock2 from "../assets/images/cool-stuff/tctc-mock-2.jpeg";
 
-import se28Front1 from "../assets/images/cool-stuff/se28-front-design-1.png";
-import se28Front2 from "../assets/images/cool-stuff/se28-front-design-2.png";
-import se28Front3 from "../assets/images/cool-stuff/se28-front-design-3.png";
-import se28Front4 from "../assets/images/cool-stuff/se28-front-design-4.png";
-import se28MockFront from "../assets/images/cool-stuff/se28-mock-front.png";
-import se28Hoodie from "../assets/images/cool-stuff/se28-hoodie.jpeg";
+// import se28Front1 from "../assets/images/cool-stuff/se28-front-design-1.png";
+// import se28Front2 from "../assets/images/cool-stuff/se28-front-design-2.png";
+// import se28Front3 from "../assets/images/cool-stuff/se28-front-design-3.png";
+// import se28Front4 from "../assets/images/cool-stuff/se28-front-design-4.png";
+// import se28MockFront from "../assets/images/cool-stuff/se28-mock-front.png";
+// import se28Hoodie from "../assets/images/cool-stuff/se28-hoodie.jpeg";
 
-import canvasFish1 from "../assets/images/cool-stuff/canvas-fish-1.jpeg";
-import canvasFish2 from "../assets/images/cool-stuff/canvas-fish-2.jpeg"
-import canvasSubway1 from "../assets/images/cool-stuff/canvas-subway-1.jpeg";
-import canvasSubway2 from "../assets/images/cool-stuff/canvas-subway-2.jpeg";
+// import canvasFish1 from "../assets/images/cool-stuff/canvas-fish-1.jpeg";
+// import canvasFish2 from "../assets/images/cool-stuff/canvas-fish-2.jpeg"
+// import canvasSubway1 from "../assets/images/cool-stuff/canvas-subway-1.jpeg";
+// import canvasSubway2 from "../assets/images/cool-stuff/canvas-subway-2.jpeg";
 
 export type CoolStuffProject = {
   slug: string;
