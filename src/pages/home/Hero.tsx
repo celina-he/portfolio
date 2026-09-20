1a9172ab4ccf4d0c1827d5f0187cd931d9c1a241
c22he <c22he@uwaterloo.ca>
2026-09-20 17:31:01 -0400

    add hero title float animation

--- src/pages/home/Hero.tsx
+++ src/pages/home/Hero.tsx
@@ -17,14 +17,15 @@ export function Hero() {
           backgroundImage: `url(${heroImage})`,
         }}
       />
+
       {/* dark image overlay */}
       <div className="absolute inset-0 bg-black/35" />
 
-      <div className="relative z-10 flex min-h-[620px] items-center justify-center px-6 text-center text-white md:min-h-[760px]">
+      <div className="relative z-10 flex min-h-[620px] items-center justify-center px-6 text-center text-white md:min-h-[760px] hero-title-float sticky font-mono">
         <div>
-          <h1 className="text-5xl font-bold tracking-wide md:text-8xl">
-            <span className="typewriter">
-                hi! i’m celina
+          <h1 className="textxl font-bold tracking-wide md:text-8xl [word-spacing:-1.5rem]">
+            <span>
+                CELINA HE
             </span>
           </h1>
           <p className="mt-4 text-2xl font-light tracking-wide md:text-3xl">
