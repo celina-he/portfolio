c70e5fc02cb6ef7ebf997e7a9af3241b9373fdc0
c22he <c22he@uwaterloo.ca>
2026-09-20 16:39:46 -0400

    added alt profile pic flip animation + text edits

--- src/pages/home/Intro.tsx
+++ src/pages/home/Intro.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 import introArt from "../../assets/images/home/sunset.png";
 import profileImage from "../../assets/images/headphones.png";
+import alternateImage from "../../assets/images/home/irl-pic.jpg";
 
 export function Intro() {
+  const [isFlipped, setIsFlipped] = useState(false);
+
   return (
     <section className="grid min-h-[520px] md:grid-cols-2">
       {/* Artwork side */}
@@ -18,11 +23,63 @@ export function Intro() {
         <div className="mx-auto w-full max-w-md">
           {/* Profile heading */}
           <div className="flex items-center gap-5">
-            <img
-              src={profileImage}
-              alt="Illustrated portrait of Celina"
-              className="h-35 w-35 rounded-full border-3 border-orange-400 object-cover"
-            />
+            <div 
+              className="relative h-35 w-35 shrink-0 cursor-pointer [perspective:1000px]"
+              onMouseEnter={() => setIsFlipped((previous) => !previous)}
+            >
+              <div
+                className={`
+                  relative
+                  h-full
+                  w-full
+                  rounded-full
+                  transition-transform
+                  duration-500
+                  [transform-style:preserve-3d]
+
+                  ${
+                    isFlipped
+                      ? "[transform:rotateX(180deg)]"
+                      : "[transform:rotateX(0deg)]"
+                  }
+                `}
+              >
+                {/* Front */}
+                <img
+                  src={profileImage}
+                  alt="Illustrated portrait of Celina"
+                  className="
+                    absolute
+                    inset-0
+                    h-full
+                    w-full
+                    rounded-full
+                    border-3
+                    border-orange-400
+                    object-cover
+                    [backface-visibility:hidden]
+                  "
+                />
+
+                {/* Back */}
+                <img
+                  src={alternateImage}
+                  alt="Alternate IRL picture of Celina"
+                  className="
+                    absolute
+                    inset-0
+                    h-full
+                    w-full
+                    rounded-full
+                    border-3
+                    border-[#356cff]
+                    object-cover
+                    [backface-visibility:hidden]
+                    [transform:rotateX(180deg)]
+                  "
+                />
+              </div>
+            </div>
 
             <div>
               <h2 className="text-4xl font-bold text-[#356cff]">
@@ -55,16 +112,19 @@ export function Intro() {
             </p>
 
             <p>
-              i’m studying{" "}
+              i’m a 3rd year student studying{" "}
               <span className="text-[#356cff]">
-                software engineering @uwaterloo
+                software engineering @ uwaterloo
               </span>{" "}
               because i had too much screen time as a kid.
             </p>
 
             <p>
-              i like to write code, draw, play roguelites, and go cafe hopping
-              to find the perfect spot to loiter.
+              i like to code, draw, and play roguelites.
+            </p>
+
+            <p>
+              You can usually find me loitering at a cafe in the area. 😎
             </p>
           </div>
         </div>
